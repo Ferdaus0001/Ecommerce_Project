2202f5d74d4ca1b4a12aea71cf1d66665f3ea1d4
Ferdaus0001 <ferdaushossan7788990@gmail.com>
2024-08-04 12:18:37 +0600

    ListView.custom

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,23 +1,52 @@
+/// Code by Ferdaus hossan
+/// Assignment No : Listview.custom
 import 'package:flutter/material.dart';
-// This is a Home Screen
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+
+class AppHome extends StatefulWidget {
+  const AppHome({super.key});
+
+  @override
+  State<AppHome> createState() => _AppHomeState();
+}
+
+class _AppHomeState extends State<AppHome> {
+  final List<String> itmes = [
+    // this is a list of Network Image
+    "https://i.ibb.co/WtRMfJt/10-Online-Admission.webp",
+    "https://i.ibb.co/HH78S1J/3-Pattern-Design-CAD.webp",
+    "https://i.ibb.co/P407CHQ/2-Apparel-Merchandising.jpg",
+    "https://i.ibb.co/KG2YnDF/5ef49e586d7185001d72ec96.jpg",
+    "https://i.ibb.co/k2p3hDC/1-Fashion-Design.jpg",
+    "https://i.ibb.co/2Wy2LRR/Fashion-Design-10-copy.jpg",
+    "https://i.ibb.co/xsNfvNg/FAJ2387.jpg",
+    "https://i.ibb.co/bLsDFW9/3.jpg",
+    "https://i.ibb.co/gt4FcT2/9-Lather-Design.jpg"
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    appBar: AppBar(
-      title: Text('Home',style: TextStyle(fontSize: 33,color: Colors.white),),
-      backgroundColor: Colors.blue,
-      centerTitle: true,
-      elevation: 10,
-    ),
-      body: Center(
-        child: Container(
-          width: 200,
-          height: 200,
-          color: Colors.blue,
-          child: Center(child: Text('Tab',style: TextStyle(fontSize: 33,color: Colors.white),)),
+      appBar: AppBar(
+        title: const Text(
+          'Custom ListView',
+          style: TextStyle(fontSize: 33, color: Colors.white),
+        ),
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+        elevation: 20,
+      ),
+      body: ListView.custom(
+        childrenDelegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+            return Card(
+              color: Colors.blue,
+              child: Padding(
+                padding: const EdgeInsets.all(3),
+                child: Image.network(itmes[index]),
+              ),
+            );
+          },
+          childCount: itmes.length,
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'HomeScreen.dart';
+ import 'HomeScreen.dart';
 void main(){
-  runApp(const EcommerceApp());
+  runApp(MYApp());
 }
 
 
-class EcommerceApp extends StatelessWidget{
-  const EcommerceApp({super.key});
+class MYApp extends StatelessWidget{
+  const MYApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-     return MaterialApp(
-       debugShowCheckedModeBanner: false,
-       home: HomeScreen(),
-     );
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AppHome(),
+
+    );
   }
 
 }
